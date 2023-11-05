902ad8d7e6cb7242903776bfe8f5ea4ff6443991
Đông nguyễn <t01639168183@gmail.com>
2023-11-05 11:47:31 +0700

    check style

--- views/components/header.php
+++ views/components/header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/index.css">
     <!-- font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- bootstrap -->
@@ -15,11 +14,14 @@
     
 
     <title>Document</title>
+    <style>
+        <?php include_once'public/css/index.css' ?>
+    </style>
 </head>
 <body>
     <header>
         <div class="h-nav">
-            <a href="index.php" class="h-logo"><img src="../project1/public/img/z4844949212064_22784bb3069c4437be7f342ef459ea24.jpg" alt="" ></a>
+            <a href="#" class="h-logo"><img src="../project1/public/img/z4844949212064_22784bb3069c4437be7f342ef459ea24.jpg" alt="" ></a>
             <ul>
                 <li><a href="#">Giày sneaker</a></li>
                 <li><a href="#">Dép</a></li>
