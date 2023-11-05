dc340d8e3a65eac73c21f0be0826c5f179e1f371
Đông nguyễn <t01639168183@gmail.com>
2023-11-05 11:42:55 +0700

    header

--- views/components/header.php
+++ views/components/header.php
@@ -12,16 +12,29 @@
     <!-- gg font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
 
     <title>Document</title>
 </head>
 <body>
-       <header>
-            <div class="h-nav">
-                <h1>header</h1>
-                <i class="fa-solid fa-house"></i>
+    <header>
+        <div class="h-nav">
+            <a href="index.php" class="h-logo"><img src="../project1/public/img/z4844949212064_22784bb3069c4437be7f342ef459ea24.jpg" alt="" ></a>
+            <ul>
+                <li><a href="#">Giày sneaker</a></li>
+                <li><a href="#">Dép</a></li>
+                <li><a href="#">Khuyến mãi</a></li>
+                <li><a href="#">Bản tin</a></li>
+            </ul>    
+            <div class="h-box-right">
+                <div class="h-search">
+                    <button class="h-btn-search"><i class="ti-search"></i>
+                    <input class="h-ip-search" type="search" placeholder="search"></button>
+                </div>
+                <a href="#" class="h-btn-user"><i class="ti-user"></i></a>
+                <a href="#" class="h-btn-cart"><i class="ti-shopping-cart"></i></a> 
             </div>
-
+        </div>
            
-       </header>
+    </header>
       
