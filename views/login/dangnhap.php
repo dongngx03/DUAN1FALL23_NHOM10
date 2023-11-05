e46ff2e09278d4ee0e040d6e07a2531ebb168bca
anhquoc <quoc290404@gmail.com>
2023-11-05 14:42:44 +0700

    đăng kí  nhập

--- views/login/dangnhap.php
+++ views/login/dangnhap.php
@@ -3,3 +3,49 @@
 <style>
     <?php include_once'public/css/login/dangnhap.css' ?>
 </style>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dangnhap.css">
+</head>
+
+<body>
+    <div class="nav">
+        <div class="h-nav">
+            <div class="logo">
+            <img src="/img/38c78da69bf34dad14e2.jpg" alt="">
+            <h1 class="Dangnhap">Đăng nhập</h1>
+        </div>
+            <div class="help">
+                <a href="#">Bạn cần giúp đỡ?</a>
+            </div>
+        </div>
+        <div class="banner-user">
+            <img src="/img/banner-sale-dich062021.jpg" alt="">
+            <div class="user">
+                <h4>Đăng nhập</h4>
+                <form action="">
+                    <input type="text" placeholder="Tên đăng nhập"><br>
+                    <input type="email" placeholder="Email đăng nhập"><br>
+                    <input type="password" placeholder="Mật khẩu">
+                </form>
+                <button>Đăng nhập</button><br>
+                <div class="q-password">
+                    <a href="#">Quên mật khẩu ?</a>
+                </div>
+                <div class="f-user">
+                   <h5>Bạn mới biết đến DLQ SHOSE ?</h5>
+                    <label for="">Đăng nhập</label>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
